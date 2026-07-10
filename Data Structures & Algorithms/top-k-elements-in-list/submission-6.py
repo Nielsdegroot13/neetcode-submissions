a4647fb4ce171da4454a492d26e8ee66d9790786
Nielsdegroot13 <nielsdg1303@gmail.com>
2026-07-10 10:34:34 +0200

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,39 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        MostK = 0
+
+        Counter = dict()
+        IndexCount = {}
+
+        BinList = [[] for _ in range(len(nums) + 1)]
+        RetList = []
+
+        for i in range(len(nums)):
+            n = nums[i]
+
+            #Count the numbers:
+            Count = Counter.get(n,0) + 1
+            Counter[n] = Count
+
+            #Store K:
+            if Count > MostK:  MostK = Count
+
+            #GetList:
+
+        for Cn in Counter:
+            Index = Cn #Get the idex?
+            Bin = Counter.get(Cn , 0) #Get the Freq to deside its bin
+            BinList[Bin].append(Index + 1000)
+
+      
+        StoredK = 0
+        for i in range(MostK,0,-1):
+            for n in range(len(BinList[i])):
+                    RetList.append(BinList[i][n] - 1000)
+                    StoredK += 1
+
+                    if StoredK >= k:
+                        return RetList
+        return RetList
+
+        
