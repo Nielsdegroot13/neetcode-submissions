91920dac46f48397be7ba2454c6cae1b8eee8c25
Nielsdegroot13 <nielsdg1303@gmail.com>
2026-07-10 10:33:59 +0200

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        Index = {}
+        for i in range(len(nums)):
+            n = nums[i]
+            if n not in Index: Index[n] = i
+            elif (n + n) == target: return [Index[n],i]
+
+
+        for i in range(len(nums)):
+            n = nums[i]
+            Needed = target - n
+
+            if Needed in Index:
+                if Index[Needed] != Index[n]:
+                    return [Index[n],Index[Needed]]
+                
+ 
+       
+
+
+
+
+
